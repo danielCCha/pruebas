cab7f44aed02ee40acbb9157b2be5b986b84f612
daniel <daniel.c.chantrero@gmail.com>
2021-10-04 18:46:40 +0200

    funcion suma de dos enteros

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,7 @@
 class Prueba{
     public static void main(String[] args) {
         System.out.println("Hola!!!!");
+        Suma s = new Suma(1,1);
+        System.out.println(s.getSuma());
     }
 }
---
+++ Sumar.java
@@ -0,0 +1,11 @@
+class Suma{
+    int a,b;
+    Suma(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+
+    int getSuma(){
+        return a+b;
+    }
+}
